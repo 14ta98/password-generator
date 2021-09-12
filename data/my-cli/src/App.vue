c0e49083b1d55406c523b249212e085de83e78d7
tishitani <ishitani@sh-concierge.com>
2021-09-12 22:25:36 +0900

    フロント周り作成（デザインなし）

--- data/my-cli/src/App.vue
+++ data/my-cli/src/App.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>{{ makeRandom() }}</div>
+  <div>
+    <header id="header">
+      <h1>Random Password Generator</h1>
+    </header>
+    <p>パスワードを自動生成するツールです。</p>
+    <dl>
+      <dt> 
+        文字
+      </dt>
+      <dd>
+        <input type="checkbox" checked>
+        <label for="uppercase">英字 (大文字)</label>
+      </dd>
+      <dd>
+        <input type="checkbox" checked>
+        <label for="lowercase">英字 (小文字)</label>
+      </dd>
+      <dd>
+        <input type="checkbox" checked>
+        <label for="number">数字</label>
+      </dd>
+      <dd>
+        <input type="checkbox" checked>
+        <label for="symbol">記号</label>
+      </dd>
+    </dl>
+      <dt>
+        文字数
+      </dt>
+      <dd>
+      <label>
+        <input type="radio" name="test">
+        <input type="number" name="test"> 文字
+      </label>
+      <label>
+        <input type="radio" name="test"> 8文字
+      </label>
+      <label>
+        <input type="radio" name="test"> 12文字
+      </label>
+      </dd>
+    <p>{{ makeRandom() }}</p>
+    <footer id="footer">
+      <small id="copyright">Copyright(C)2021 Wrapper All right reserved.</small>
+    </footer>
+  </div>
 </template>
 
-<script>  
+<script>
 import randomatic from "randomatic";
 export default {
   props: {
@@ -18,7 +63,7 @@ export default {
   methods: {
     makeRandom() {
       var arr = [];
-      for (let i = 0; i < 8; i++) {
+      for (let i = 0; i < 12; i++) {
         arr.push(randomatic(this.pattern, this.length));
       }
       return arr;
