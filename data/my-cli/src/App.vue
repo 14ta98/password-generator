70a4ba0328c22becebb6a7805eeb3f2bb731ddbc
tishitani <takuya_ishitani@outlook.com>
2021-09-25 15:05:39 +0900

    vuetifyを追加

--- data/my-cli/src/App.vue
+++ data/my-cli/src/App.vue
@@ -1,106 +1,60 @@
 <template>
-  <div>
-    <header id="header">
-      <h1>Random Password Generator</h1>
-    </header>
-    <p>パスワードを自動生成するツールです。</p>
-    <form>
-    </form>
-    <dl>
-      <dt>
-        文字
-      </dt>
-      <dd>
-        <input type="checkbox" id="uppercase" v-model="uppercase">
-        <label for="uppercase">英字 (大文字)</label>
-      </dd>
-      <dd>
-        <input type="checkbox" id="lowercase" v-model="lowercase">
-        <label for="lowercase">英字 (小文字)</label>
-      </dd>
-      <dd>
-        <input type="checkbox"  id="number" v-model="number">
-        <label for="number">数字</label>
-      </dd>
-      <dd>
-        <input type="checkbox" id="symbol" v-model="symbol">
-        <label for="symbol">記号</label>
-      </dd>
-    </dl>
-      <dt>
-        文字数
-      </dt>
-      <dd>
-        <input type="radio" name="test" @change="changeLength('length8')"> 8文字
-        <input type="radio" name="test" @change="changeLength('length12')"> 12文字
-        <input type="radio" name="test" @change="changeLength('free')">
-        <input type="number" v-model="characterLength"> 文字
-      </dd>
-    <div v-for="(r,index) in makeRandom" :key="index">
-      {{r}}
-    </div>
-    <footer id="footer">
-      <small id="copyright">Copyright(C)2021 XXX All right reserved.</small>
-    </footer>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import randomatic from "randomatic";
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  data() {
-    return {
-      uppercase: true,
-      lowercase: true,
-      number: true,
-      symbol: true,
-      lengthFree: false,
-      length8: false,
-      length12: false,
-      characterLength: 0,
-    };
+  name: 'App',
+
+  components: {
+    HelloWorld,
   },
-  computed: {
-    makeRandom() {
-      var arr = [];
-      for (let i = 0; i < 12; i++) {
-        arr.push(randomatic(this.createCharacterPattern, this.createCharacterLength));
-      }
-      return arr;
-    },
-    createCharacterPattern() {
-      const petterns = [];
-      if (this.uppercase) petterns.push("A");
-      if (this.lowercase) petterns.push("a");
-      if (this.number) petterns.push("0");
-      if (this.symbol) petterns.push("!");
-      return petterns.join('');
-    },
-    createCharacterLength() {
-      if (this.lengthFree) return this.characterLength;
-      if (this.length8) return 8;
-      if (this.length12) return 12;
-      return 0;
-    },
-  },
-  methods: {
-    changeLength(val) {
-      if (val === 'free') {
-        this.lengthFree = true;
-        this.length8 = false;
-        this.length12 = false;
-      }
-      if (val === 'length8') {
-        this.lengthFree = false;
-        this.length8 = true;
-        this.length12 = false;
-      }
-      if (val === 'length12') {
-        this.lengthFree = false;
-        this.length8 = false;
-        this.length12 = true;
-      }
-    },
-  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
