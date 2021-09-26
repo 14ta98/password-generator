22ddcd6c58d99f1a2d052f77ea57579e9ab593a5
tishitani <takuya_ishitani@outlook.com>
2021-09-27 00:16:15 +0900

    vuetifyで組み込む

--- data/my-cli/src/App.vue
+++ data/my-cli/src/App.vue
@@ -3,81 +3,111 @@
     <header>
       <v-app-bar app color="primary" dark flat>
         <v-container class="py-0 fill-height">
-          <v-toolbar-title>Random Password</v-toolbar-title>
+          <v-toolbar-title>RandomPassword</v-toolbar-title>
         </v-container>
       </v-app-bar>
     </header>
-    <v-main class="grey lighten-4">
+    <v-main class="grey lighten-3">
       <v-container>
         <v-row>
           <v-col>
-            <v-card flat>
-              <v-card-text>
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+              <v-card-text flat>
                 <v-container>
-                  <!-- <v-row>
-                    <v-col cols="12" sm="4" md="4">
-                      <v-checkbox
-                        v-model="ex4"
-                        label="英字（大文字）"
-                        color="primary"
-                        value="uppercase"
-                        hide-details
-                      ></v-checkbox>
-                    </v-col>
-                  </v-row> -->
                   <v-row>
-                    <label for="character-type">文字の種類</label>
-                    <v-col cols="12" sm="2" md="2">
+                    <!-- <v-col><label for="">文字</label></v-col> -->
+                    <v-col>
                       <v-checkbox
-                        v-model="ex4"
-                        label="英字 (大文字)"
+                        v-model="uppercase"
+                        label="大文字"
                         color="primary"
-                        value="uppercase"
                         hide-details
                       ></v-checkbox>
                     </v-col>
-                    <v-col cols="12" sm="2" md="2">
+                    <v-col>
                       <v-checkbox
-                        v-model="ex4"
-                        label="英字 (小文字)"
+                        v-model="lowercase"
+                        label="小文字"
                         color="primary"
-                        value="lowercase"
                         hide-details
                       ></v-checkbox>
                     </v-col>
-                    <v-col cols="12" sm="2" md="2">
+                    <v-col>
                       <v-checkbox
-                        v-model="ex4"
+                        v-model="number"
                         label="数字"
                         color="primary"
-                        value="number"
                         hide-details
                       ></v-checkbox>
                     </v-col>
-                    <v-col cols="12" sm="2" md="2">
+                    <v-col>
                       <v-checkbox
-                        v-model="ex4"
+                        v-model="symbol"
                         label="記号"
                         color="primary"
-                        value="symbol"
                         hide-details
                       ></v-checkbox>
                     </v-col>
                   </v-row>
-                  <v-radio-group v-model="row" row>
-                    <label for="character-type">文字数</label>
-                    <v-radio label="8文字" value="radio-1"></v-radio>
-                    <v-radio label="12文字" value="radio-2"></v-radio>
-                    <v-radio label="カスタム" value="radio-3"></v-radio>
+                  <v-radio-group row>
+                    <v-radio label="8文字"> </v-radio>
+                    <v-radio label="12桁"></v-radio>
+                    <v-radio label="カスタム"></v-radio>
+                    <v-flex xs12 sm6 md3>
+                      <v-text-field
+                        type="number"
+                        step="any"
+                        min="0"
+                        max="20"
+                        ref="input"
+                        v-model.number="number"
+                      ></v-text-field>
+                    </v-flex>
                   </v-radio-group>
+                  <div v-for="(r, index) in makeRandom" :key="index">
+                    {{ r }}
+                  </div>
+
+                  <!-- <v-col><label for="">文字数</label></v-col> -->
+                  <!-- <v-radio-group row>
+                  <v-radio label="8文字" value="radio-1"></v-radio>
+                  <v-radio label="12桁" value="radio-2"></v-radio>
+                  <v-radio label="カスタム" value="radio-3"></v-radio>
+                  <v-flex xs12 sm6 md3>
+                    <v-text-field
+                      type="number"
+                      step="any"
+                      min="0"
+                      max="20"
+                      ref="input"
+                      v-model.number="number"
+                    ></v-text-field>
+                  </v-flex>
+                </v-radio-group> -->
                 </v-container>
               </v-card-text>
-            </v-card>
-
-            <!-- <v-sheet min-height="70vh" rounded="lg"> -->
-            <!--  -->
-            <!-- </v-sheet> -->
+            </v-sheet>
           </v-col>
+          <!-- <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-content>
+                    <v-list-item-title> List Item {{ n }} </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item link color="grey lighten-4">
+                  <v-list-item-content>
+                    <v-list-item-title> Refresh </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col> -->
         </v-row>
       </v-container>
     </v-main>
@@ -90,4 +120,62 @@
 </template>
 
 <script>
+import randomatic from "randomatic";
+export default {
+  data() {
+    return {
+      uppercase: true,
+      lowercase: true,
+      number: true,
+      symbol: true,
+      // lengthFree: false,
+      // length8: false,
+      // length12: false,
+      // characterLength: 0,
+    };
+  },
+  computed: {
+    makeRandom() {
+      var arr = [];
+      for (let i = 0; i < 12; i++) {
+        arr.push(randomatic(this.createCharacterPattern));
+      }
+      return arr;
+    },
+    createCharacterPattern() {
+      const petterns = [];
+      if (this.uppercase) petterns.push("A");
+      if (this.lowercase) petterns.push("a");
+      if (this.number) petterns.push("0");
+      if (this.symbol) petterns.push("!");
+      console.log(petterns);
+      return petterns.join("");
+    },
+    createCharacterLength() {
+      // if (this.lengthFree) return this.characterLength;
+      // if (this.length8) return 8;
+      // if (this.length12) return 12;
+      return 0;
+    },
+  },
+  methods: {
+    changeLength(val) {
+      if (val === "free") {
+        this.lengthFree = true;
+        this.length8 = false;
+        this.length12 = false;
+      }
+      if (val === "length8") {
+        this.lengthFree = false;
+        this.length8 = true;
+        this.length12 = false;
+      }
+      if (val === "length12") {
+        this.lengthFree = false;
+        this.length8 = false;
+        this.length12 = true;
+      }
+    },
+  },
+};
 </script>
